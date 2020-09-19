b7e240e030b84042f2a067defe68f5028f7808e5
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-09-19 08:02:18 +0200

    add queries and mutations for links

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-  type Query{
-    info:String!
-    feed:[Link!]!
-  }
-  type Link{
-    id:ID!
-    description:String!
-    url:String!
-  }
-`;
-
 let links = [
   {
     id: "link-0",
@@ -20,20 +8,47 @@ let links = [
   },
 ];
 
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews clone`,
     feed: () => links,
+    link: (parent, args) => {
+      return links.find((l) => l.id === args.id);
+    },
   },
   // Link: {
   //   // id: (parent) => parent.id,
   //   // description: (parent) => parent.description,
   //   // url: (parent) => parent.url,
   // },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      let link = links.find((l) => l.id === args.id);
+      link.url = args.url;
+      link.description = args.description;
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      let idx = links.findIndex((link) => link.id === args.id);
+      let link = links[idx];
+      links.splice(idx, 1);
+      return link;
+    },
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 
