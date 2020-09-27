d29e14c67669e66f489b551f85e91b7c2582ed62
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-09-27 19:33:50 +0200

    switch database to prisma and connect to graphql

--- src/index.js
+++ src/index.js
@@ -1,76 +1,34 @@
 const { GraphQLServer } = require("graphql-yoga");
-require("./db/mongoose");
-const Link = require("./models/link");
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL",
-  },
-];
-
-let idCount = links.length;
+const { PrismaClient } = require("@prisma/client");
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews clone`,
+    info: () => `This is the API of a Hackernews Clone`,
     feed: async (parent, args, context) => {
-      return context.Link.find({});
-    },
-    link: async (parent, args, context) => {
-      return context.Link.findOne({ _id: args.id });
+      return context.prisma.link.findMany();
     },
   },
-  // Link: {
-  //   // id: (parent) => parent.id,
-  //   // description: (parent) => parent.description,
-  //   // url: (parent) => parent.url,
-  // },
   Mutation: {
-    post: async (parent, args, context, info) => {
-      const link = new context.Link({
-        description: args.description,
-        url: args.url,
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
       });
-      await link.save();
-
-      return link;
-    },
-    updateLink: async (parent, args, context) => {
-      let res = await context.Link.updateOne(
-        { _id: args.id },
-        { url: args.url, description: args.description }
-      );
-      let link = await context.Link.findOne({ _id: args.id });
-      return link;
-    },
-    deleteLink: async (parent, args, context) => {
-      let link = await context.Link.findOne({ _id: args.id });
-      let res = await context.Link.deleteOne({ _id: args.id });
-      return link;
+      return newLink;
     },
   },
 };
+
+const prisma = new PrismaClient();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
   context: {
-    Link,
+    prisma,
   },
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
-
-async function tst() {
-  const args = {
-    url: "asdf",
-    description: "adsf",
-    id: "5f663564865af9db43fef757",
-  };
-  let res = await Link.deleteOne({ _id: args.id });
-  console.log(res);
-}
-
-//tst();
--- src/script.js
+++ src/script.js
@@ -1,24 +1,27 @@
-require("./db/mongoose");
+// 1
+const { PrismaClient } = require("@prisma/client");
 
-const Link = require("./models/link");
+// 2
+const prisma = new PrismaClient();
 
-// const testLink = new Link({
-//   url: "example3333.com",
-//   description: "url 3",
-//   wut: "bruv",
-// });
-
-// testLink
-//   .save()
-//   .then(() => {
-//     console.log(testLink);
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
+//3
 async function main() {
-  const links = await Link.find({});
-  console.log(links);
+  const newLink = await prisma.link.create({
+    data: {
+      description: "adf",
+      url: "asaf",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
 }
 
-main();
+//4
+main()
+  .catch((e) => {
+    throw e;
+  })
+  // 5
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
